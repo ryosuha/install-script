b8f6b7bc556801a390cf3a2cec7b8a56b4ada530
ryosuha <52070765+ryosuha@users.noreply.github.com>
2021-12-02 15:18:59 +0900

    Update cni-canal-install-script.sh

--- k8s/cni-canal-install-script.sh
+++ k8s/cni-canal-install-script.sh
@@ -3,6 +3,6 @@ set -e
 
 curl https://docs.projectcalico.org/manifests/canal.yaml -O
 cat canal.yaml | sed -e "s/policy\/v1beta1/policy\/v1/g" >canalrev.yaml
-su - $SUDO_USER -c "kubectl apply -f canalrev.yaml"
+su $SUDO_USER -c "kubectl apply -f canalrev.yaml"
 
 exit 0
