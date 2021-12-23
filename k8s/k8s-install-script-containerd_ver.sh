d653c64891f5161ac931ed1a175cba73f45951de
ryosuha <52070765+ryosuha@users.noreply.github.com>
2021-12-23 13:10:41 +0900

    Create k8s-install-script-containerd_ver.sh

---
+++ k8s/k8s-install-script-containerd_ver.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+set -e
+
+install_ubuntu2004() {
+    kOS=xUbuntu_20.04
+    kVERSION=1.21:1.21.0
+    kVERSION1=1.21
+    kVERSIONLONG=1.21.0-00
+
+    apt update
+    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+    echo 'overlay' >> /etc/modules-load.d/crio.conf
+    echo 'br_netfilter' >> /etc/modules-load.d/crio.conf
+    modprobe overlay
+    modprobe br_netfilter
+
+    echo 'net.bridge.bridge-nf-call-iptables  = 1' >> /etc/sysctl.d/99-kubernetes-cri.conf
+    echo 'net.ipv4.ip_forward                 = 1' >> /etc/sysctl.d/99-kubernetes-cri.conf
+    echo 'net.bridge.bridge-nf-call-ip6tables = 1' >> /etc/sysctl.d/99-kubernetes-cri.conf
+    sysctl --system
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+    apt-get update
+    apt-get install -y containerd.io
+
+    mkdir -p /etc/containerd
+    containerd config default | tee /etc/containerd/config.toml
+
+    systemctl restart containerd
+
+    echo 'br_netfilter' >> /etc/modules-load.d/k8s.conf
+
+    echo 'net.bridge.bridge-nf-call-ip6tables = 1' >> /etc/sysctl.d/k8s.conf
+    echo 'net.bridge.bridge-nf-call-iptables = 1' >> /etc/sysctl.d/k8s.conf
+
+    sysctl --system
+
+    apt update
+    
+    curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+    echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
+    apt update
+
+    apt install -y kubelet=$kVERSIONLONG kubeadm=$kVERSIONLONG kubectl=$kVERSIONLONG
+
+    apt-mark hold kubelet kubeadm kubectl
+
+    swapoff /swap.img
+    echo '#!/bin/bash' > /etc/rc.local
+    echo 'swapoff /swap.img' >> /etc/rc.local
+    echo 'exit 0' >> /etc/rc.local
+    chmod +x /etc/rc.local
+
+    kubeadm init --pod-network-cidr=172.24.0.0/16
+
+    USER_HOME=$(bash -c "cd ~$(printf %q $SUDO_USER) && pwd")
+    mkdir -p $USER_HOME/.kube
+    cp -i /etc/kubernetes/admin.conf $USER_HOME/.kube/config
+    chown $SUDO_UID:$SUDO_GID $USER_HOME/.kube/config
+
+    su $SUDO_USER -c "kubectl taint nodes $HOSTNAME node-role.kubernetes.io/master-"
+
+}
+
+
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   echo "Sample sudo ./frr-install-script.sh" >&2
+   exit 1
+fi
+
+if [ ${SUDO_USER} ]; then
+  echo ${SUDO_USER}
+else
+  echo "Run from Root User? Prefer to run from normal user with sudo command" >&2
+  echo "Sample sudo ./frr-install-script.sh" >&2
+  exit 1
+fi
+
+OS=`lsb_release -i | awk -F':' '{print $2}' | sed -e "s/\s//g"`
+VERSION=`lsb_release -r | awk -F':' '{print $2}' | sed -e "s/\s//g"`
+
+echo $OS
+echo $VERSION
+
+case ${OS} in
+  "Ubuntu")
+    case ${VERSION} in
+      "20.04")
+        install_ubuntu2004
+        ;;
+      "18.04")
+        #install_ubuntu1804
+        echo "1804 Not Supported Yet"
+        ;;
+      *)
+        echo "NO VERSION MATCH"
+        ;;
+    esac
+    ;;
+  *)
+    echo "NO OS MATCH"
+    ;;
+esac
+
