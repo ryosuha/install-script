21cfb418667513101ee87be2ed6ed56259a08313
ryosuha <ryosunata@gmail.com>
2022-05-06 06:14:52 +0000

    ubuntu 22.04 update

--- frr-install-script.sh
+++ frr-install-script.sh
@@ -1,13 +1,91 @@
 #!/bin/bash
 set -e
 
-install_ubuntu2004() {
-    apt update
+install_ubuntu2204() {
+    apt-get update
     apt-get install -y git autoconf automake libtool make libreadline-dev texinfo pkg-config libpam0g-dev libjson-c-dev bison flex libc-ares-dev python3-dev python3-sphinx install-info build-essential libsnmp-dev perl libcap-dev python2 libelf-dev libunwind-dev
 
     apt-get install -y protobuf-c-compiler libprotobuf-c-dev
     apt-get install -y libzmq5 libzmq3-dev
-    apt install -y python-is-python3
+    apt-get install -y python-is-python3
+
+    curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+    python2 ./get-pip.py
+
+    sudo apt-get install -y libyang2-dev
+
+    groupadd -r -g 92 frr
+    groupadd -r -g 85 frrvty
+    adduser --system --ingroup frr --home /var/run/frr/ --gecos "FRR suite" --shell /sbin/nologin frr
+    usermod -a -G frrvty frr
+
+    git clone https://github.com/frrouting/frr.git frr
+    cd frr
+    ./bootstrap.sh
+    ./configure \
+        --prefix=/usr \
+        --includedir=\${prefix}/include \
+        --bindir=\${prefix}/bin \
+        --sbindir=\${prefix}/lib/frr \
+        --libdir=\${prefix}/lib/frr \
+        --libexecdir=\${prefix}/lib/frr \
+        --localstatedir=/var/run/frr \
+        --sysconfdir=/etc/frr \
+        --with-moduledir=\${prefix}/lib/frr/modules \
+        --with-libyang-pluginsdir=\${prefix}/lib/frr/libyang_plugins \
+        --enable-configfile-mask=0640 \
+        --enable-logfile-mask=0640 \
+        --enable-snmp=agentx \
+        --enable-multipath=64 \
+        --enable-user=frr \
+        --enable-group=frr \
+        --enable-vty-group=frrvty \
+        --with-pkg-git-version \
+        --with-pkg-extra-version=-MyOwnFRRVersion
+    make
+    make install
+
+    install -m 775 -o frr -g frr -d /var/log/frr
+    install -m 775 -o frr -g frrvty -d /etc/frr
+    install -m 640 -o frr -g frrvty tools/etc/frr/vtysh.conf /etc/frr/vtysh.conf
+    install -m 640 -o frr -g frr tools/etc/frr/frr.conf /etc/frr/frr.conf
+    install -m 640 -o frr -g frr tools/etc/frr/daemons.conf /etc/frr/daemons.conf
+    install -m 640 -o frr -g frr tools/etc/frr/daemons /etc/frr/daemons
+
+    echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+    echo 'net.ipv6.conf.all.forwarding=1' >> /etc/sysctl.conf
+    sysctl -p
+
+    echo 'mpls_router' >> /etc/modules-load.d/modules.conf
+    echo 'mmpls_iptunnels' >> /etc/modules-load.d/modules.conf
+    modprobe mpls-router mpls-iptunnel
+
+    #enabling mpls on interface
+    list=`ip link | awk -F'[:]' '{print $2}' | grep ens`
+    ens=`echo $list | tr -s " "`
+    for arg in $ens
+    do
+        echo 'net.mpls.conf.'$arg'.input=1' >> /etc/sysctl.conf
+    done
+
+    echo 'net.mpls.platform_labels=100000' >> /etc/sysctl.conf
+
+    pwd=`pwd`
+    install -m 644 $pwd/tools/frr.service /etc/systemd/system/frr.service
+    systemctl enable frr
+
+    sed -i 's/=no/=yes/' /etc/frr/daemons
+
+    systemctl start frr
+}
+
+install_ubuntu2004() {
+    apt-get update
+    apt-get install -y git autoconf automake libtool make libreadline-dev texinfo pkg-config libpam0g-dev libjson-c-dev bison flex libc-ares-dev python3-dev python3-sphinx install-info build-essential libsnmp-dev perl libcap-dev python2 libelf-dev libunwind-dev
+
+    apt-get install -y protobuf-c-compiler libprotobuf-c-dev
+    apt-get install -y libzmq5 libzmq3-dev
+    apt-get install -y python-is-python3
 
     curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
     python2 ./get-pip.py
@@ -82,6 +160,7 @@ install_ubuntu2004() {
 
     systemctl start frr
 }
+
 if ! [ $(id -u) = 0 ]; then
    echo "The script need to be run as root." >&2
    echo "Sample sudo ./frr-install-script.sh" >&2
@@ -105,6 +184,9 @@ echo $VERSION
 case ${OS} in
   "Ubuntu")
     case ${VERSION} in
+      "22.04")
+        install_ubuntu2204
+        ;;
       "20.04")
         install_ubuntu2004
         ;;
